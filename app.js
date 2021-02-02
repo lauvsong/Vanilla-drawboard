abc5730bc5d69b209747226bdeb0054695525d45
lauvsong <hyehye9355@naver.com>
2021-02-03 02:30:06 +0900

    Add button switch on filling mode

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -10,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startPainting(){
     painting = true;
@@ -41,6 +43,16 @@ function handleRangeChange(event){
     ctx.lineWidth = size;
 }
 
+function handleModeClick(event){
+    if (filling){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
 if (canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -54,4 +66,8 @@ Array.from(colors).forEach(color =>
 
 if (range){
     range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
 }
